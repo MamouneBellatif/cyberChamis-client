87c48ebc0d8c8a908339002143ab9d77c551c665
l3miage-bouserhj <jeanbouserhal@outlook.com>
2022-05-02 16:54:10 +0200

    qlq modifs ici et là

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,6 +22,7 @@ import { HttpClientModule } from '@angular/common/http';
 import {MatInputModule} from '@angular/material/input';
 import {MatButtonModule} from '@angular/material/button';
 import {MatTableModule} from '@angular/material/table';
+import {MatCardModule} from '@angular/material/card';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
@@ -48,6 +49,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     MatInputModule,
     MatButtonModule,
     MatTableModule,
+    MatCardModule,
     BrowserAnimationsModule,
   ],
   providers: [],
