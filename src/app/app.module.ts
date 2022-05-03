e8da659256081d95accfd19f4262ad3967b32a72
l3miage-bouserhj <jeanbouserhal@outlook.com>
2022-05-03 11:30:18 +0200

    mise en page : casse-tout refait-tout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,6 +23,7 @@ import {MatInputModule} from '@angular/material/input';
 import {MatButtonModule} from '@angular/material/button';
 import {MatTableModule} from '@angular/material/table';
 import {MatCardModule} from '@angular/material/card';
+import {MatToolbarModule} from '@angular/material/toolbar';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ListElementChamiComponent } from './list-element-chami/list-element-chami.component';
 
@@ -52,6 +53,7 @@ import { ListElementChamiComponent } from './list-element-chami/list-element-cha
     MatButtonModule,
     MatTableModule,
     MatCardModule,
+    MatToolbarModule,
     BrowserAnimationsModule,
   ],
   providers: [],
