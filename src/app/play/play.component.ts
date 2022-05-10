113f2944e1196ea920d8beb68f8d4014c9eb08ce
l3miage-bonninal <bonnin.alice@gmail.com>
2022-05-10 10:57:04 +0200

    calcul scores

--- src/app/play/play.component.ts
+++ src/app/play/play.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { CyberchamisService, Reponse, Visite } from '../cyberchamis.service';
+import { CyberchamisService, Etape, Reponse, TypeEtape, Visite } from '../cyberchamis.service';
 
 @Component({
   selector: 'app-play',
@@ -43,7 +43,6 @@ export class PlayComponent implements OnInit {
     this.visiteId = this.route.snapshot.paramMap.get('visiteId') || '';
     this.chamiId = this.route.snapshot.paramMap.get('chamiId') || '';
     this.ccService.getVisite(this.visiteId).then(visite => this.visite = visite);
-    
   }
 
   retour(){
@@ -51,12 +50,33 @@ export class PlayComponent implements OnInit {
     this.router.navigateByUrl("/");
   }
 
-  fin(){
-    //compile and display final score
-    const maxScore: number = this.visite.defi.etape.reduce<number>((total, etape) => {
-      return total += etape.point;
-    }, 0)
-    //const score: number = 
+  fin(fini: boolean){
+    const reponsesAttendues = this.visite.defi.etape.reduce((questions, etape) => {
+      if(etape.type_etape === TypeEtape.question){
+        questions.push(etape)
+      }
+      return questions
+    }, [] as Etape[]);
+    
+    if(reponsesAttendues.length === this.reponses.length){ // on a une réponse à toutes les questions
+      //compile and display final score
+      const maxScore: number = reponsesAttendues.reduce<number>((total, etape) => {
+        return total += etape.point;
+      }, 0);
+
+      reponsesAttendues.sort((a, b) => a.id-b.id); // devrait être inutile mais sait-on jamais
+      this.reponses.sort((a, b) => a.question_id-b.question_id);
+
+      const score = this.reponses.reduce( (score, reponse, iR) => {
+        return score += reponse.valide ? reponsesAttendues[iR].point : 0;
+      }, 0);
+
+      //Afficher fin 
+      console.log(score + "/" + maxScore)     
+    }
+    else{
+      console.log("questions: " + this.visite.defi.etape.length + ", reponses: "+ this.reponses.length)
+    }
   }
 
   
