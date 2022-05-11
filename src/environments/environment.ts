1f64b8c8a6c3b8b0bf3e3107dc3a57717cccd6f4
l3miage-bellatim <bellatif@yahoo.fr>
2022-05-11 16:21:14 +0200

    s

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -14,7 +14,7 @@ export const environment = {
   },
   production: false,
   // apiUrl: 'http://localhost:8080/api/'
-  apiUrl: 'https://projet-integrateur-g5.herokuapp.com/'
+  apiUrl: 'https://projet-integrateur-g5.herokuapp.com/api/'
 };
 
 /*
